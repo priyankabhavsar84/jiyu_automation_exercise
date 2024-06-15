3630feb78c71e25ce99f723ed270200ab47b420e
priyankabhavsar84 <161707474+priyankabhavsar84@users.noreply.github.com>
2024-06-15 20:43:17 +0100

    test

--- src/main/java/account/Account.java
+++ src/main/java/account/Account.java
@@ -1,4 +1,41 @@
 package account;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Account {
+    private String accountNumber;
+    private String accountName;
+    private double balance;
+    private List<String> transactions;
+
+    public Account(String accountNumber, String accountName) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = 0.0;
+        this.transactions = new ArrayList<>();
+    }
+
+    public void deposit(String transactionId, double amount) {
+        transactions.add(transactionId);
+        balance += amount;
+    }
+
+    public void withdraw(String transactionId, double amount) {
+        transactions.add(transactionId);
+        balance -= amount;
+    }
+
+    public String generateStatement() {
+        StringBuilder statement = new StringBuilder();
+        statement.append("Name: ").append(accountName).append("\n");
+        statement.append("Account: ").append(accountNumber).append("\n");
+        statement.append("Balance: ").append(String.format("%.2f", balance)).append("\n");
+        statement.append("Transactions: ").append("\n");
+        for (String transaction : transactions) {
+            statement.append(transaction).append("\n");
+        }
+        return statement.toString();
+    }
 }
--- src/test/java/glue/AccountSteps.java
+++ src/test/java/glue/AccountSteps.java
@@ -2,15 +2,59 @@ package glue;
 
 import account.Account;
 import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
 
 public class AccountSteps {
 
-    Account account = null;
 
-    @Given("^Account exists for Acc No\\. \"([^\"]*)\" with Name \"([^\"]*)\"$")
-    public void accountExistsForAccNoWithName(String number, String name) {
-        throw new PendingException();
+
+        private Account account;
+        private String statement;
+
+        @Given("Account exists for Acc No. {string} with Name {string}")
+        public void accountExists(String accountNumber, String name) {
+            account = new Account(accountNumber, name);
+        }
+
+        @Given("deposits are made")
+        public void depositsAreMade(io.cucumber.datatable.DataTable dataTable) {
+            Map<String, Double> deposits = dataTable.asMap(String.class, Double.class);
+            for (Map.Entry<String, Double> deposit : deposits.entrySet()) {
+                account.deposit(deposit.getKey(), deposit.getValue());
+            }
+        }
+
+        @Given("withdrawals are made")
+        public void withdrawalsAreMade(io.cucumber.datatable.DataTable dataTable) {
+            Map<String, Double> withdrawals = dataTable.asMap(String.class, Double.class);
+            for (Map.Entry<String, Double> withdrawal : withdrawals.entrySet()) {
+                account.withdraw(withdrawal.getKey(), withdrawal.getValue());
+            }
+        }
+
+        @When("statement is produced")
+        public void statementIsProduced() {
+            statement = account.generateStatement();
+        }
+
+        @Then("statement includes {string}")
+        public void statementIncludes(String expectedContent) {
+            assertTrue(statement.contains(expectedContent));
+        }
+
+    @And("withdrawls are made")
+    public void withdrawlsAreMade(io.cucumber.datatable.DataTable dataTable) {
+        Map<String, Double> withdrawals = dataTable.asMap(String.class, Double.class);
+        for (Map.Entry<String, Double> withdrawal : withdrawals.entrySet()) {
+            account.withdraw(withdrawal.getKey(), withdrawal.getValue());
+        }
+
     }
-
 }
