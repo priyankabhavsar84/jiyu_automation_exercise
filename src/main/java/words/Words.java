535116488a1dd1a784dd3d2b9a49c575ea511158
priyankabhavsar84 <161707474+priyankabhavsar84@users.noreply.github.com>
2024-06-18 14:59:19 +0100

    test

--- src/main/java/words/Words.java
+++ src/main/java/words/Words.java
@@ -2,8 +2,21 @@ package words;
 
 import java.util.List;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Words {
     public static List<String> getUniqueWordsFromSentence(String sentence) {
-        throw new UnsupportedOperationException();
+        try {
+            return Arrays.stream(sentence.toLowerCase()
+                            .split("[\\s;.,:\\!\\?]+"))
+                    .distinct()
+                    .collect(Collectors.toList());
+        } catch (UnsupportedOperationException e) {
+            throw e;
+        }
     }
 }
