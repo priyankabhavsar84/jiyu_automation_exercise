db6547474034e628f58e1b78f17b9978db996961
priyankabhavsar84 <161707474+priyankabhavsar84@users.noreply.github.com>
2024-06-19 13:39:35 +0100

    test

--- src/main/java/account/Account.java
+++ src/main/java/account/Account.java
@@ -6,4 +6,39 @@ import java.util.List;
 
 public class Account {
 
-}
+        private String accountNumber;
+        private String accountName;
+        static int balance;
+        private static List<String> transactions;
+
+        public Account(String accountNumber, String accountName) {
+            this.accountNumber = accountNumber;
+            this.accountName = accountName;
+            this.balance = 0 ;
+            this.transactions = new ArrayList<>();
+        }
+
+        public static void deposit(String account, double amount) {
+            transactions.add(account);
+            balance += amount;
+        }
+
+        public void withdraw(String transactionId, double amount) {
+            transactions.add(transactionId);
+            balance -= amount;
+        }
+
+        public String generateStatement() {
+            StringBuilder statement = new StringBuilder();
+            statement.append("Name: ").append(accountName).append("\n");
+            statement.append("Account: ").append(accountNumber).append("\n");
+            statement.append("Balance: ").append(String.format("%.2f", balance)).append("\n");
+            statement.append("Transactions: ").append("\n");
+            for (String transaction : transactions) {
+                statement.append(transaction).append("\n");
+            }
+            return statement.toString();
+        }
+    }
+
+
--- src/test/java/glue/AccountSteps.java
+++ src/test/java/glue/AccountSteps.java
@@ -11,8 +11,55 @@ import java.util.Map;
 
 public class AccountSteps {
 
+        private int balance;
+        String accountName;
+        String accountNumber;
+
+        @Given("^Account exists for Acc No\\. \"([^\"]*)\" with Name \"([^\"]*)\"$")
+        public void accountExistsForAccNoWithName(String number, String name) {
+            accountName= name;
+            accountNumber = number;
+        }
+
+
+
+        @Given("deposits are made")
+        public void deposits_are_made(DataTable dataTable) {
+            List<Map<String, String>> deposits = dataTable.asMaps(String.class, String.class);
+            for (Map<String, String> deposit : deposits) {
+                int amount = Integer.parseInt(deposit.get("amount"));
+                String account = deposit.get("account");
+                System.out.println("account"+account);
+                System.out.println("amount"+amount);
+                Account.deposit(account,amount);
+            }
+
+        }
+        @Given("withdrawls are made")
+        public void withdrawls_are_made(DataTable dataTable) {
+            List<Map<String, String>> deposits = dataTable.asMaps(String.class, String.class);
+            for (Map<String, String> deposit : deposits) {
+                int amount = Integer.parseInt(deposit.get("amount"));
+                balance -= amount;
+            }
+        }
+        @When("statement is produced")
+        public void statement_is_produced() {
+            System.out.println("print balance==="+balance);
+        }
+        @Then("statement includes {string}")
+        public void statement_includes(String string) {
+
+        }
 
 
 
 
-}
+    }
+
+
+
+
+
+
+
